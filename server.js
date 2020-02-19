3964b618a990088d870e11697c71f1a18c09e359
shwetakelkar <shweta4.gupta@gmail.com>
2020-02-19 11:43:34 -0600

    changes made for contact

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const bodyParser = require('body-parser');
+const sgMail = require('@sendgrid/mail');
 const app = express();
 const PORT = process.env.PORT || 3000;
+var mailApiKey = process.env.SENDGRID_API_KEY
+sgMail.setApiKey(mailApiKey);
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -9,6 +12,23 @@ app.use(express.static("public"));
 
 app.get("/",function(req,res){
     res.render("index");
+})
+
+app.post("/sendmail",function(req,res){
+    console.log(req.body.email);
+    const msg = {
+        to:"shweta4.gupta@gmail.com",
+        from:req.body.email,
+        html:req.body.message+" "+req.body.name
+    }
+    sgMail.send(msg).then(() => {
+        res.send("success!")
+      })
+      .catch((err) => {
+        console.error(err);
+        res.send("An error occured");
+      });;
+
 })
 
 app.listen(PORT, () => {
