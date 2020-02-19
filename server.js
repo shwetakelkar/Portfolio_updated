9d9c9a709e12b20d8f28cfc585a3f8a4c1dc87dd
shwetakelkar <shweta4.gupta@gmail.com>
2020-02-19 11:04:33 -0600

    Sever coding

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const bodyParser = require('body-parser');
 const app = express();
-const port = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
 
 app.get("/",function(req,res){
-    res.redirect("index");
+    res.render("index");
 })
 
-app.listen(PORT, (err) => {
-	console.log(err || `Server running on port ${PORT}.`)
+app.listen(PORT, () => {
+	console.log(`Server running on port ${PORT}.`)
 })
